e510ee6e875d9452062e1ab3d804ea5973d7e7c0
Ira Hulieieva <iryna.topol@gmail.com>
2021-02-02 15:11:59 +0200

    commit new 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -24,6 +24,7 @@ public class Main {
         String[] split = strInt.split("");
         System.out.println();
         System.out.println("GIT");
+        System.out.println("GIT");
         int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         IntStream.range(1, 10)
                 .filter(s -> s % 2 == 0)
